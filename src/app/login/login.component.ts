042c1c1351942fcd7418079d911c45bd378765fb
Moses Ubah <39748749+mohzys23@users.noreply.github.com>
2025-01-15 01:14:55 +0100

    implmented auth endpoint

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { InputComponent } from '../components/shared/input/input.component';
 import { FormControl } from '@angular/forms';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -15,4 +16,24 @@ import { MatPseudoCheckboxModule } from '@angular/material/core';
 export class LoginComponent {
   emailControl = new FormControl("")
   passwordControl = new FormControl('');
+
+  constructor(private http: HttpClient) {}
+
+
+  onLogin (event: Event) {
+    event.preventDefault();
+
+    const userData = {
+      email: this.emailControl.value,
+      password: this.passwordControl.value,
+    }
+
+    this.http.post('http://localhost:3030/auth/login', userData, {
+      headers: {
+        'X-Api-Key': "I'M_A_FRONTEND_DEVELOPER_AND_I_WANT_TO_JOIN_THE_TEAM",
+      }
+    }).subscribe((response) => {
+       console.log("login response:", response)
+    })
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { InputComponent } from '../components/shared/input/input.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -19,5 +20,30 @@ export class SignupComponent {
   repeatpasswordControl = new FormControl('');
   nameControl = new FormControl('');
   lastNameControl = new FormControl('');
+
+  constructor(private http: HttpClient) {}
+
+
+  onSubmit(event: Event) {
+    event.preventDefault()
+
+
+    const userData = {
+      name: this.nameControl.value,
+      lastname: this.lastNameControl.value,
+      email: this.emailControl.value,
+      password: this.passwordControl.value,
+      repassword: this.repeatpasswordControl.value
+    }
+
+
+    this.http.post("http://localhost:3030/auth/register", userData, {
+      headers: {
+        'X-Api-Key': "I'M_A_FRONTEND_DEVELOPER_AND_I_WANT_TO_JOIN_THE_TEAM",
+      }
+}).subscribe((response) => {
+      console.log('signup response:', response);
+    })
+  }
 }
 

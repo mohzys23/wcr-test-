d96d3d8f315c8d9ea11b9a779993d57c19d27d65
Moses Ubah <39748749+mohzys23@users.noreply.github.com>
2025-01-16 22:08:47 +0100

    updated ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default redirect
@@ -16,13 +14,5 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
     ],
-  },
-  {
-    path: 'dashboard',
-    component: DashboardLayoutComponent,
-    children: [
-      { path: '', component: DashboardComponent },
-    ],
-  },
-  { path: '**', component: PagenotfoundComponent }, // 404 handling
+  }
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { AlertComponent } from './components/alert/alert.component';
 import { ButtonSpinnerComponent } from './components/button-spinner/button-spinner.component';
@@ -8,7 +7,7 @@ import { ButtonSpinnerComponent } from './components/button-spinner/button-spinn
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AlertComponent, ButtonComponent, InputComponent, ButtonSpinnerComponent], // Import standalone component here
-  exports: [AlertComponent, ButtonComponent, InputComponent, ButtonSpinnerComponent], // Export if needed elsewhere
+  imports: [CommonModule, AlertComponent, InputComponent, ButtonSpinnerComponent], // Import standalone component here
+  exports: [AlertComponent, InputComponent, ButtonSpinnerComponent], // Export if needed elsewhere
 })
 export class SharedModule { }
